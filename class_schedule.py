29259d8a426e657a073b2231a031595734b19c79
Steven Ke <spp72kimo@gmail.com>
2022-01-07 02:19:14 +0800

    modify send_sms.py SMS able to send

--- class_schedule.py
+++ class_schedule.py
@@ -3,6 +3,7 @@ import time
 
 day = time.strftime("%d", time.localtime())			# 當前日期
 date = time.strftime("%Y-%m-%d", time.localtime())
+hour_specify = 8									# 指定每天八點傳訊息
 wb = load_workbook('202201.xlsx')					# 讀取班表Excel
 sheet = wb['10011']									# 選擇工作表sheet
 
@@ -33,11 +34,10 @@ for row in range(6,8):
 		status = '休假'
 	schedule_h2[name.value] = status
 
-print('今天日期', date)	
-print('H2的班表：')
-for n in schedule_h2:
-	print(n,schedule_h2[n])
-
-print('H3的班表：')
+sentence_h3 = 'H3今日班表：' + str(date) + '\n'
 for n in schedule_h3:
-	print(n,schedule_h3[n])
+	sentence_h3 += str(n) + ' ' + str(schedule_h3[n]) + '\n'
+
+sentence_h2 = 'H2今日班表：' + str(date) + '\n'
+for n in schedule_h2:
+	sentence_h2 += str(n) + ' ' + str(schedule_h2[n]) + '\n'
--- send_sms.py
+++ send_sms.py
@@ -1,16 +1,31 @@
-# /usr/bin/env python
-# Download the twilio-python library from twilio.com/docs/libraries/python
-import os
-from twilio.rest import Client
+from twilio.rest import Client 
+from class_schedule import *
+ 
+account_sid = 'ACfde6f9072071bb544f06c5d38b6a8bfc' 
+auth_token = '6eb26590c0ebd7e5818d0559f05746fd' 
+client = Client(account_sid, auth_token) 
 
-# Find these values at https://twilio.com/user/account
-# To set up environmental variables, see http://twil.io/secure
-account_sid = os.environ['ACfde6f9072071bb544f06c5d38b6a8bfc']
-auth_token = os.environ['20596bf550c25fd86b7db1dd28c5cc35']
+i = 0
+hour_spec = 8
+while True:
+    cur_sec = time.time()
+    cur_time = time.localtime(cur_sec)
+    if cur_time.tm_hour == hour + 1:
+        i = 0
+    if cur_time.tm_hour == hour:
+        if i == 1:
+            continue
+        message = client.messages.create(  
+                              messaging_service_sid='MG1194dae546fa5791ccb7cd0630f86fa2', 
+                              body=sentence_h3,      
+                              to='+886938876892' 
+                          ) 
+        print(message.sid)
 
-client = Client(account_sid, auth_token)
-
-client.api.account.messages.create(
-    to="+12316851234",
-    from_="+15555555555",
-    body="Hello there!")
+        message = client.messages.create(  
+                              messaging_service_sid='MG1194dae546fa5791ccb7cd0630f86fa2', 
+                              body=sentence_h2,      
+                              to='+886938876892' 
+                          ) 
+        print(message.sid)
+        i = 1
