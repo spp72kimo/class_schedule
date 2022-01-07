4355cdfd9c406d8670f66699f044a797aa5bcc0a
Steven Ke <spp72kimo@gmail.com>
2022-01-08 02:30:33 +0800

    time changed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ auth_token = '6eb26590c0ebd7e5818d0559f05746fd'
 client = Client(account_sid, auth_token) 
 
 i = 0               # 讓訊息只發一次的開關
-hour_spec = 8       # 指定發送訊息時間
+hour_spec = 32       # 指定發送訊息時間
 while True:
     cur_sec = time.time()
     cur_time = time.localtime(cur_sec)
