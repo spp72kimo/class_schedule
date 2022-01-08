c2197b5bf73fb4818493b82cd3bb1f865b312f15
Steven Ke <spp72kimo@gmail.com>
2022-01-08 20:59:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,22 +15,22 @@ def send_msg():
     while True:
         cur_sec = time.time()
         cur_time = time.localtime(cur_sec)
-        if cur_time.tm_min == hour_spec + 1:
-            i = 0
-        if cur_time.tm_min == hour_spec:
-            if i == 1:
-                continue
-            message = client.messages.create(  
-                                      messaging_service_sid='MG1194dae546fa5791ccb7cd0630f86fa2', 
-                                      body=sentence_h3,      
-                                      to='+886938876892' 
-                                  ) 
-            print(message.sid)
+        # if cur_time.tm_min == hour_spec + 1:
+        #     i = 0
+        # if cur_time.tm_min == hour_spec:
+        #     if i == 1:
+        #         continue
+        message = client.messages.create(  
+                                  messaging_service_sid='MG1194dae546fa5791ccb7cd0630f86fa2', 
+                                  body=sentence_h3,      
+                                  to='+886938876892' 
+                              ) 
+        print(message.sid)
 
-            message = client.messages.create(  
-                                      messaging_service_sid='MG1194dae546fa5791ccb7cd0630f86fa2', 
-                                      body=sentence_h2,      
-                                      to='+886938876892' 
-                                  ) 
-            print(message.sid)
-            i = 1
+        message = client.messages.create(  
+                                  messaging_service_sid='MG1194dae546fa5791ccb7cd0630f86fa2', 
+                                  body=sentence_h2,      
+                                  to='+886938876892' 
+                              ) 
+        print(message.sid)
+        i = 1
