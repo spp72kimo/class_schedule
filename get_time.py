0c9db6e91e48d6c962d97ac0c86eb913b69d51f5
Steven Ke <spp72kimo@gmail.com>
2022-01-10 22:05:54 +0800

    Rename and delete file

--- get_time.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime,timezone,timedelta
-dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
-dt2 = dt1.astimezone(timezone(timedelta(hours=8))) # 轉換時區 -> 東八區
-
-print('UTC \t%s\nUTC+8\t%s'%(dt1,dt2))
-print(dt2.strftime("%Y-%m-%d %H:%M:%S")) # 將時間轉換為 string
--- test.py
+++
@@ -1,7 +0,0 @@
-Dict_sample=  {
-
-  'KEY': 'order',
-  'VALUE': 99
-}
-Dict_sample_str = str(Dict_sample)
-print(Dict_sample_str)
